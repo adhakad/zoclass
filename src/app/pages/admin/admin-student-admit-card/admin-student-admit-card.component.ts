c05f0aef2e21b71cdab0fe0f685e1ab92787df69
Abhishek Dhakad <dhakaddeepak9340700360@gmail.com>
2024-02-28 10:18:51 +0530

    fix tc and admin admit card module

--- src/app/pages/admin/admin-student-admit-card/admin-student-admit-card.component.ts
+++ src/app/pages/admin/admin-student-admit-card/admin-student-admit-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MatRadioChange } from '@angular/material/radio';
 import { AdmitCardStructureService } from 'src/app/services/admit-card-structure.service';
 import { AdmitCardService } from 'src/app/services/admit-card.service';
 import { PrintPdfService } from 'src/app/services/print-pdf/print-pdf.service';
@@ -27,6 +28,7 @@ export class AdminStudentAdmitCardComponent implements OnInit {
   selectedStream: string = '';
   streamSection: boolean = true;
   streamMainSubject: any[] = [];
+  selectedValue: number = 0;
   constructor(public activatedRoute: ActivatedRoute, private schoolService: SchoolService, private admitCardService: AdmitCardService, private printPdfService: PrintPdfService, private admitCardStructureService: AdmitCardStructureService) {
 
   }
@@ -39,6 +41,10 @@ export class AdminStudentAdmitCardComponent implements OnInit {
     var currentURL = window.location.href;
     this.baseURL = new URL(currentURL).origin;
   }
+
+  onChange(event: MatRadioChange) {
+    this.selectedValue = event.value;
+  }
   closeModal() {
     this.showModal = false;
     this.streamSection = true;
@@ -46,7 +52,7 @@ export class AdminStudentAdmitCardComponent implements OnInit {
     this.processedData = [];
   }
   bulkPrint() {
-    if (this.cls <= 10 || this.cls == 200 || this.cls==201 || this.cls == 202) {
+    if (this.cls <= 10 || this.cls == 200 || this.cls == 201 || this.cls == 202) {
       this.selectedStream = 'N/A';
       this.admitCardStrInfoByStream = this.admitCardStrInfo.filter((item: any) => item.stream === 'N/A');
       this.processData(this.selectedStream);
@@ -63,7 +69,7 @@ export class AdminStudentAdmitCardComponent implements OnInit {
     this.admitCardStructureService.admitCardStructureByClass(cls).subscribe((res: any) => {
       if (res) {
         this.admitCardStrInfo = res;
-        for(let i=0;i<res.length;i++){
+        for (let i = 0; i < res.length; i++) {
           this.streamMainSubject.push(res[i].stream);
         }
       }
@@ -77,70 +83,17 @@ export class AdminStudentAdmitCardComponent implements OnInit {
     })
   }
   printStudentData() {
-    const printContent = this.getPrintContent();
-    this.printPdfService.printContent(printContent);
+    if (this.selectedValue==0) {
+      const printContent = this.getPrintOneAdmitCardContent();
+      this.printPdfService.printContent(printContent);
+    }
+    if(this.selectedValue==1){
+      const printContent = this.getPrintTwoAdmitCardContent();
+      this.printPdfService.printContent(printContent);
+    }
     this.closeModal();
   }
-  // private getPrintContent(): string {
-  //   let schoolName = this.schoolInfo.schoolName;
-  //   let city = this.schoolInfo.city;
-  //   let printHtml = '<html>';
-  //   printHtml += '<head>';
-  //   printHtml += '<style>';
-  //   printHtml += 'body { margin: 0; padding: 0; }';
-  //   printHtml += 'div { margin: 0; padding: 0;}';
-  //   printHtml += '.custom-container {font-family: Arial, sans-serif;overflow: auto;}';
-  //   printHtml += '.table-container {background-color: #fff;border: none;}';
-  //   printHtml += '.school-name {display: flex; align-items: center; justify-content: center; text-align: center; }';
-  //   printHtml += '.address{margin-left:130px;margin-top: -65px;}';
-  //   printHtml += '.address p{margin-top: -5px !important;}';
-  //   printHtml += '.logo { height: 100px;margin-right:10px; }';
-  //   printHtml += '.school-name h3 { color: #2e2d6a !important; font-size: 20px !important; margin-left: 15px;margin-top:-65px !important; margin-bottom: 0 !important; }';
-  //   printHtml += '.info-table {width:100%;color: #2e2d6a !important;border: none;font-size: 12px;letter-spacing: .25px;margin-top: 5vh;margin-bottom: 5vh;display: inline-table;}';
-  //   printHtml += '.table-container .info-table th, .table-container .info-table td{color: #2e2d6a !important;text-align:center}';
-  //   printHtml += '.title-lable {max-height: 45px;text-align: center;margin-bottom: 15px;border:1px solid #2e2d6a;border-radius: 5px;margin-top: 25px;}';
-  //   printHtml += '.title-lable p {color: #2e2d6a !important;font-size: 15px;font-weight: 500;letter-spacing: 1px;}';
-  //   printHtml += '.codes .school-code  {margin-right:65%;}';
-  //   printHtml += '.custom-table {width: 100%;color: #2e2d6a !important;border-collapse:collapse;font-size: 12px;letter-spacing: .25px;margin-bottom: 20px;display: inline-table;border-radius:5px}';
-  //   printHtml += '.custom-table th{height:38px;text-align: center;border:1px solid #2e2d6a;}';
-  //   printHtml += '.custom-table tr{height:38px;}';
-  //   printHtml += '.custom-table td {text-align: center;border:1px solid #2e2d6a;}';
-  //   printHtml += '.text-bold { font-weight: bold;}';
-  //   printHtml += 'p {color: #2e2d6a !important;font-size:12px;}'
-  //   printHtml += 'h4 {color: #2e2d6a !important;}'
-  //   printHtml += '@media print {';
-  //   printHtml += '  body::before {';
-  //   printHtml += `    content: "${schoolName}, ${city}";`;
-  //   printHtml += '    position: fixed;';
-  //   printHtml += '    top: 45%;';
-  //   printHtml += '    left:10%;';
-  //   printHtml += '    font-size: 20px;';
-  //   printHtml += '    font-weight: bold;';
-  //   printHtml += '    font-family: Arial, sans-serif;';
-  //   printHtml += '    color: rgba(0, 0, 0, 0.08);';
-  //   printHtml += '    pointer-events: none;';
-  //   printHtml += '  }';
-  //   printHtml += '}';
-  //   printHtml += '</style>';
-  //   printHtml += '</head>';
-  //   printHtml += '<body>';
-
-  //   this.allAdmitCards.forEach((student, index) => {
-  //     const studentElement = document.getElementById(`student-${student.studentId}`);
-  //     if (studentElement) {
-  //       printHtml += studentElement.outerHTML;
-
-  //       // Add a page break after each student except the last one
-  //       if (index < this.allAdmitCards.length - 1) {
-  //         printHtml += '<div style="page-break-after: always;"></div>';
-  //       }
-  //     }
-  //   });
-  //   printHtml += '</body></html>';
-  //   return printHtml;
-  // }
-
-  private getPrintContent(): string {
+  private getPrintOneAdmitCardContent(): string {
     let schoolName = this.schoolInfo.schoolName;
     let city = this.schoolInfo.city;
     let printHtml = '<html>';
@@ -150,12 +103,13 @@ export class AdminStudentAdmitCardComponent implements OnInit {
     printHtml += 'div { margin: 0; padding: 0;}';
     printHtml += '.custom-container {font-family: Arial, sans-serif;overflow: auto;}';
     printHtml += '.table-container {background-color: #fff;border: none;}';
+    printHtml += '.logo { height: 75px;margin-right:10px; }';
     printHtml += '.school-name {display: flex; align-items: center; justify-content: center; text-align: center; }';
-    printHtml += '.address{margin-left:130px;margin-top: -65px;}';
-    printHtml += '.address p{margin-top: -5px !important;}';
-    printHtml += '.logo { height: 100px;margin-right:10px; }';
-    printHtml += '.school-name h3 { color: #2e2d6a !important; font-size: 20px !important; margin-left: 15px;margin-top:-65px !important; margin-bottom: 0 !important; }';
-    printHtml += '.info-table {width:100%;color: #2e2d6a !important;border: none;font-size: 12px;letter-spacing: .25px;margin-top: 5vh;margin-bottom: 5vh;display: inline-table;}';
+    printHtml += '.school-name h3 { color: #2e2d6a !important; font-size: 18px !important;margin-top:-120px !important; margin-bottom: 0 !important; }';
+    printHtml += '.address{margin-top: -45px;}';
+    printHtml += '.address p{margin-top: -10px !important;}';
+
+    printHtml += '.info-table {width:100%;color: #2e2d6a !important;border: none;font-size: 12px;letter-spacing: .25px;margin-top: 1.5vh;margin-bottom: 2vh;display: inline-table;}';
     printHtml += '.table-container .info-table th, .table-container .info-table td{color: #2e2d6a !important;text-align:center}';
     printHtml += '.title-lable {max-height: 45px;text-align: center;margin-bottom: 15px;border:1px solid #2e2d6a;border-radius: 5px;margin-top: 25px;}';
     printHtml += '.title-lable p {color: #2e2d6a !important;font-size: 15px;font-weight: 500;letter-spacing: 1px;}';
@@ -171,7 +125,67 @@ export class AdminStudentAdmitCardComponent implements OnInit {
     printHtml += '  body::before {';
     printHtml += `    content: "${schoolName}, ${city}";`;
     printHtml += '    position: fixed;';
-    printHtml += '    top: 45%;';
+    printHtml += '    top: 40%;';
+    printHtml += '    left:10%;';
+    printHtml += '    font-size: 20px;';
+    printHtml += '    font-weight: bold;';
+    printHtml += '    font-family: Arial, sans-serif;';
+    printHtml += '    color: rgba(0, 0, 0, 0.08);';
+    printHtml += '    pointer-events: none;';
+    printHtml += '  }';
+    printHtml += '}';
+    printHtml += '</style>';
+    printHtml += '</head>';
+    printHtml += '<body>';
+
+    this.allAdmitCards.forEach((student, index) => {
+      const studentElement = document.getElementById(`student-${student.studentId}`);
+      if (studentElement) {
+        printHtml += studentElement.outerHTML;
+
+        // Add a page break after each student except the last one
+        if (index < this.allAdmitCards.length - 1) {
+          printHtml += '<div style="page-break-after: always;"></div>';
+        }
+      }
+    });
+    printHtml += '</body></html>';
+    return printHtml;
+  }
+
+  private getPrintTwoAdmitCardContent(): string {
+    let schoolName = this.schoolInfo.schoolName;
+    let city = this.schoolInfo.city;
+    let printHtml = '<html>';
+    printHtml += '<head>';
+    printHtml += '<style>';
+    printHtml += 'body { margin: 0; padding: 0; }';
+    printHtml += 'div { margin: 0; padding: 0;}';
+    printHtml += '.custom-container {font-family: Arial, sans-serif;overflow: auto;}';
+    printHtml += '.table-container {background-color: #fff;border: none;}';
+    printHtml += '.school-name {display: flex; align-items: center; justify-content: center; text-align: center; }';
+    printHtml += '.address{margin-top: -46px;}';
+    printHtml += '.address p{margin-top: -10px !important;}';
+    printHtml += '.logo { height: 75px;margin-right:10px; }';
+    printHtml += '.school-name h3 { color: #2e2d6a !important; font-size: 18px !important;margin-top:-120px !important; margin-bottom: 0 !important; }';
+    printHtml += '.info-table {width:100%;color: #2e2d6a !important;border: none;font-size: 12px;letter-spacing: .25px;margin-top: 5px;margin-bottom: 15px;display: inline-table;}';
+    printHtml += '.table-container .info-table th, .table-container .info-table td{color: #2e2d6a !important;text-align:center}';
+    printHtml += '.title-lable {text-align: center;border-radius: 5px;margin-top:10px;margin-bottom:10px;}';
+    printHtml += '.title-lable p {color: #2e2d6a !important;font-size: .9rem;font-weight: 500;letter-spacing: .8px;}';
+    printHtml += '.codes .school-code  {margin-right:65%;}';
+    printHtml += '.custom-table {width: 100%;color: #2e2d6a !important;border-collapse:collapse;font-size: 12px;letter-spacing: .25px;margin-bottom: 20px;display: inline-table;border-radius:5px;}';
+    printHtml += '.custom-table th{height:26px;text-align: center;border:1px solid #2e2d6a;}';
+    printHtml += '.custom-table tr{height:26px;}';
+    printHtml += '.custom-table td {text-align: center;border:1px solid #2e2d6a;}';
+    printHtml += '.text-bold { font-weight: bold;}';
+    printHtml += 'p {color: #2e2d6a !important;font-size:12px;}'
+    printHtml += 'h4 {color: #2e2d6a !important;}'
+    printHtml += '.watermark { position: fixed;font-family: Arial, sans-serif; font-size: 20px; font-weight: bold; color: rgba(0, 0, 0, 0.03); top: 75%; left:10%; pointer-events: none; z-index: 1; }';
+    printHtml += '@media print {';
+    printHtml += '  body::before {';
+    printHtml += `    content: "${schoolName}, ${city}";`;
+    printHtml += '    position: fixed;';
+    printHtml += '    top: 25%;';
     printHtml += '    left:10%;';
     printHtml += '    font-size: 20px;';
     printHtml += '    font-weight: bold;';
@@ -187,10 +201,11 @@ export class AdminStudentAdmitCardComponent implements OnInit {
     for (let i = 0; i < this.allAdmitCards.length; i += 2) {
       const student1 = this.allAdmitCards[i];
       const student2 = i + 1 < this.allAdmitCards.length ? this.allAdmitCards[i + 1] : null;
-  
+
+      printHtml += `<div class="watermark">${schoolName}, ${city}</div>`;
       // Print details for the first student
       printHtml += this.getStudentHtml(student1);
-  
+
       // Add a page break after the first student if there is a second student
       if (student2) {
         printHtml += '<div style="page-break-after: always;"></div>';
@@ -198,12 +213,12 @@ export class AdminStudentAdmitCardComponent implements OnInit {
         printHtml += this.getStudentHtml(student2);
       }
     }
-  
+
     printHtml += '</body></html>';
     return printHtml;
   }
 
-  private getStudentHtml(student:any): string {
+  private getStudentHtml(student: any): string {
     const studentElement = document.getElementById(`student-${student.studentId}`);
     if (studentElement) {
       return studentElement.outerHTML;
@@ -251,7 +266,8 @@ export class AdminStudentAdmitCardComponent implements OnInit {
               fatherName: studentInfo.fatherName,
               motherName: studentInfo.motherName,
               rollNumber: studentInfo.rollNumber,
-              admissionNo: studentInfo.admissionNo
+              admissionNo: studentInfo.admissionNo,
+              session: studentInfo.session
             });
           }
 
